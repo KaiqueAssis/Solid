f12502d05c91bcfa558dd48675c0dc2e42ec2c8d
kaiquessis <kaiqueassis12k@gmail.com>
2023-01-24 20:50:18 -0300

    colocar o readme do dip no github

--- src/srp/App.java
+++ src/srp/App.java
@@ -1,7 +1,33 @@
 package srp;
 
+import java.util.Scanner;
+import java.util.Locale;
+
 public class App {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+        Account ac = new Account();
+
+        System.out.println("Digite o numero para criar a sua conta.");
+        int account = sc.nextInt();
+        ac.setAccount(account);
+
+
+        System.out.println("Digite o seu nome");
+        String name = sc.next();
+        ac.setNameAccount(name);
+
+        System.out.println("Vai ter deposito inicial? sim/nao");
+        String confirm = sc.next();
+
+        if(confirm.equals("sim")){
+            System.out.println("Valor do aporte inicial");
+            double firstDeposit = sc.nextDouble();
+            ac.setDeposit(firstDeposit);
+        } else {
+            System.out.println("ok");
+        }
 
     }
 }
